d2d89d8e9f949ca5fc04ce2b4863765b478b3d7d
Kev <ksayad@gmail.com>
2023-12-19 23:19:13 -0500

    first com

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning rust with too many linked lists
+
+Training resource: https://rust-unofficial.github.io/too-many-lists/index.html
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+pub mod first;
+pub mod second;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::first::List;
+
+    #[test]
+    fn basics() {
+
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+
+
+    }
+}
